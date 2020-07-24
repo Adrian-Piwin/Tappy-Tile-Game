6de41a89e6935794d3b240a2ff0ec9ffd11c587e
Adrian <adrian7048@gmail.com>
2020-07-24 14:30:05 -0400

    Created Grid
    Started project and created dynamically generated grid filled with sprite instants.

---
+++ Tile Rythm Game/Assets/Scripts/GridManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    private int rows = 5;
+    [SerializeField]
+    private int cols = 5;
+    [SerializeField]
+    private float tileSize = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateGrid()
+    {
+        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("tile-0"));
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+
+                float posX = col * tileSize;
+                float posY = row * -tileSize;
+
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+
+        Destroy(referenceTile);
+
+        float gridW = cols * tileSize;
+        float gridH = rows * tileSize;
+        transform.position = new Vector2(-gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);
+    }
+}
