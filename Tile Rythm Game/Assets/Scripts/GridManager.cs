9287c8f5f289d4bdc015de108a3a274840c3d1bc
Adrian <adrian7048@gmail.com>
2020-07-24 22:44:15 -0400

    Tile Collision and Communication
    Able to now detect collision with a tile when clicked, also able to tell which exact tile was clicked

--- Tile Rythm Game/Assets/Scripts/GridManager.cs
+++ Tile Rythm Game/Assets/Scripts/GridManager.cs
@@ -11,32 +11,53 @@ public class GridManager : MonoBehaviour
     [SerializeField]
     private float tileSize = 1;
 
+    private GameObject[,] gridArray;
+    private List<GameObject> queuedTileOrder = new List<GameObject>();
+    private GameObject currentClickedTile;
+
+    public bool tileClicked = false;
+
     // Start is called before the first frame update
     void Start()
     {
         GenerateGrid();
+        StartGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (tileClicked)
+        {
+            if (currentClickedTile == queuedTileOrder[0]){
+                Debug.Log("Yes");
+            }else{
+                Debug.Log("No");
+            }
+
+            tileClicked = false;
+        }
     }
 
     private void GenerateGrid()
     {
         GameObject referenceTile = (GameObject)Instantiate(Resources.Load("tile-0"));
+        gridArray = new GameObject[rows, cols];
 
         for (int row = 0; row < rows; row++)
         {
+
             for (int col = 0; col < cols; col++)
             {
                 GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+                tile.name = row + "," + col;
 
                 float posX = col * tileSize;
                 float posY = row * -tileSize;
 
                 tile.transform.position = new Vector2(posX, posY);
+
+                gridArray[row, col] = tile;
             }
         }
 
@@ -45,5 +66,16 @@ public class GridManager : MonoBehaviour
         float gridW = cols * tileSize;
         float gridH = rows * tileSize;
         transform.position = new Vector2(-gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);
+    }
+
+    private void StartGame()
+    {
+        queuedTileOrder.Add(gridArray[2,3]);
+    }
+
+    public void updateTileClicked(GameObject tile)
+    {
+        tileClicked = true;
+        currentClickedTile = tile;
     }
 }
---
+++ Tile Rythm Game/Assets/Scripts/TileScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    private GridManager gridManager;
+
+    void Start()
+    {
+        gridManager = GameObject.FindObjectOfType<GridManager>();
+    }
+
+    void OnMouseDown(){
+        passTile();
+    }
+
+    void OnMouseEnter(){
+        if (Input.GetMouseButton(0)){
+            passTile();
+        }
+    }
+
+    void passTile(){
+        gridManager.updateTileClicked(gameObject);
+    }
+}
